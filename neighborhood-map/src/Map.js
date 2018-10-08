6835473a88f04f4106a120cc3e549e77baaebefe
ZumDeWald <zumdewald@gmail.com>
2018-10-07 22:23:17 -0500

    Multiple markers display, marker animates
    Map centers around all markers
    5 Markers display
    Markers animate onto Map and bounce when clicked
    Info window displays when clicked

--- neighborhood-map/src/Map.js
+++ neighborhood-map/src/Map.js
@@ -7,8 +7,10 @@ class Map extends Component {
   componentWillReceiveProps ({ isScriptLoadSucceed }) {
     if (isScriptLoadSucceed) {
 
+      const markers = [];
+
       const map = new window.google.maps.Map(document.getElementById('map'), {
-        center: {lat: 38.7091572, lng: -90.3872404},
+        center: {lat: 34.6466411, lng: -97.9536852},
         zoom: 13
       });
 
@@ -20,21 +22,60 @@ class Map extends Component {
         {title: 'location5', location:{lat: 34.6667351, lng: -97.9548392}},
       ]
 
-      let newPoint = {lat: 38.7048806, lng:-90.355353};
-      let marker = new window.google.maps.Marker({
-        position: newPoint,
-        map: map,
-        title: 'NumberOne'
+      let infoWindow = new window.google.maps.InfoWindow({
+        content: null
       });
 
-      let infoWindow = new window.google.maps.InfoWindow({
-        content: `I am telling you the INFO`
-      });
-      marker.addListener("click", () => {
-        infoWindow.open(map, marker);
-      });
+      let bounds = new window.google.maps.LatLngBounds();
+
+      //Loop over locations and create markers
+      for (let i=0; i<locations.length; i++) {
+        //get position from location array
+        let position = locations[i].location;
+        let title = locations[i].title;
+
+        //create a marker per location and push to array
+        let marker = new window.google.maps.Marker({
+          map: map,
+          position: position,
+          title: title,
+          animation: window.google.maps.Animation.DROP,
+          id: i
+        });
+
+        //push into markers array
+        markers.push(marker);
+
+        //extend bounds of map for each marker so they all appear on load
+        bounds.extend(marker.position);
+
+        //create onClick event to open InfoWindow
+        marker.addListener('click', function() {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE);
+          populateInfoWindow(this, infoWindow);
+        });
+      }
+
+      //fit map to bounds of all markers
+      map.fitBounds(bounds);
+
+      function populateInfoWindow(marker, infowindow) {
+        //make sure infoWindow not already open
+        if (infowindow.marker !== marker) {
+          infowindow.marker = marker;
+          infowindow.setContent(`<div>${marker.title}</div>`);
+          infowindow.open(map, marker);
+
+          //make sure marker is cleared if infowindow is closed
+          infowindow.addListener('closeclick', function() {
+            infowindow.setContent(null);
+            marker.setAnimation(null);
+          });
+        }
+      }
 
     } else {
+      //alert if the script does not load
       alert('No Script Loaded')
     }
   }
