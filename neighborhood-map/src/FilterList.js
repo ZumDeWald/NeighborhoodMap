75ccdf7566a2e26a76c0ee7c48de18d9147954b6
ZumDeWald <zumdewald@gmail.com>
2018-10-14 14:31:43 -0500

    Fixed bug with infoWindows populating in list
    InfoWindow text was getting mixed with IDs from list
    and populating the text in the list vs window

--- neighborhood-map/src/FilterList.js
+++ neighborhood-map/src/FilterList.js
@@ -6,11 +6,11 @@ function FilterList(props) {
   <div id="filter-list">
     <button className="drop-button">Filter</button>
     <div id="drop-list" className="list-style">
-      <span id="1">Loc1</span>
-      <span id="2">Loc2</span>
-      <span id="3">Loc3</span>
-      <span id="4">Loc4</span>
-      <span id="5">Loc5</span>
+      <span id="first">Loc1</span>
+      <span id="second">Loc2</span>
+      <span id="third">Loc3</span>
+      <span id="fourth">Loc4</span>
+      <span id="fifth">Loc5</span>
     </div>
   </div>
   )
