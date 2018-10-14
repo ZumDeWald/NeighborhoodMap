a54d70c448d88c53a4f51dbd5e7ab32a7db9e5de
ZumDeWald <zumdewald@gmail.com>
2018-10-14 15:33:43 -0500

    Filter setup, needs to push to map
    Filter func written and can filter list, but still will
    no push re render to map markers

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -14,13 +14,51 @@ class App extends Component {
       {title: 'Nazareth', location:{lat: 32.6996454, lng: 35.2908666}},
       {title: 'Capernaum', location:{lat: 32.8803473, lng: 35.5645522}},
       {title: 'Gesthsemane', location:{lat: 31.7793143, lng: 35.2375914}},
-      {title: 'Church of the Holy Sepulchre', location:{lat: 31.777207, lng: 35.231681}},
+      {title: 'Church of the Holy Sepulchre', location:{lat: 31.777207, lng: 35.231681}}
     ],
+
+    showLocations: [
+      {title: 'Bethlehem', location:{lat: 31.7053996, lng: 35.1936877}},
+      {title: 'Nazareth', location:{lat: 32.6996454, lng: 35.2908666}},
+      {title: 'Capernaum', location:{lat: 32.8803473, lng: 35.5645522}},
+      {title: 'Gesthsemane', location:{lat: 31.7793143, lng: 35.2375914}},
+      {title: 'Church of the Holy Sepulchre', location:{lat: 31.777207, lng: 35.231681}}
+    ]
   }
 
-  filterLocations (point) {
-    const updatedLocation = this.state.locations.filter(location => location === point);
-    this.setState({ locations: updatedLocation });
+  filterLocations = (point) => {
+    this.setState( state => ({
+      showLocations: state.locations.filter(location => location.title === point)
+    }));
+  }
+
+    //On creating a map instance, add markers/infoWindows
+    //This setup allows for multiple Maps to be loaded
+    //independently inside the same main App if desired
+  mapLoad = (map) => {
+    //Create bounds instance
+    const bounds = new window.google.maps.LatLngBounds();
+
+    //Loop over state and create marker info for each location
+    //Then add listener for each individual marker
+    this.state.showLocations.forEach((place, index) => {
+      let position = place.location;
+      let title = place.title;
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: index
+      });
+      //Extend boundry of map to incorporate each marker
+      bounds.extend(marker.position);
+      marker.addListener('click', () => {
+        this.createInfoWindow(marker, map);
+      });
+    });
+    //Fit map to extended bounds
+    map.fitBounds(bounds);
   }
 
   createInfoWindow(e, map) {
@@ -48,50 +86,19 @@ class App extends Component {
 
   render() {
 
-    let markers = [];
-    const locations = this.state.locations;
-
     return (
       <div id="map-container">
-        <FilterList />
+        <FilterList
+          locations={this.state.locations}
+          onFilterLocations={this.filterLocations}
+         />
         <Map
           id='main-map'
           options={{
             center: { lat:31.7053996 ,lng:35.1936877 },
             zoom: 13
           }}
-
-        //On creating a map instance, add markers/infoWindows
-        //This setup allows for multiple Maps to be loaded
-        //independently inside the same main App if desired
-          onMapLoad={ map => {
-            //Create bounds instance
-            const bounds = new window.google.maps.LatLngBounds();
-
-            //Loop over state and create marker info for each
-            //location and push into new array
-            //Then add listener for each individual marker
-            locations.forEach((place, index) => {
-              let position = place.location;
-              let title = place.title;
-              let marker = new window.google.maps.Marker({
-                map: map,
-                position: position,
-                title: title,
-                animation: window.google.maps.Animation.DROP,
-                id: index
-              });
-              //Add marker to new array
-              markers.push(marker);
-              //Extend boundry of map to incorporate each marker
-              bounds.extend(marker.position);
-              marker.addListener('click', () => {
-                this.createInfoWindow(marker, map);
-              });
-            });
-            //Fit map to extended bounds
-            map.fitBounds(bounds);
-          }}
+          onMapLoad={ this.mapLoad }
         />
       </div>
     );
--- neighborhood-map/src/FilterList.js
+++ neighborhood-map/src/FilterList.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 function FilterList(props) {
+
+  // const generateList = (locations) => {
+  //   locations.forEach((location) => {
+  //     return <span id="location.title">location.title</span>
+  //   })
+  // }
+
   return(
     //Empty div to be populated by props passed down
   <div id="filter-list">
     <button className="drop-button">Filter</button>
     <div id="drop-list" className="list-style">
-      <span id="first">Loc1</span>
-      <span id="second">Loc2</span>
-      <span id="third">Loc3</span>
-      <span id="fourth">Loc4</span>
-      <span id="fifth">Loc5</span>
+      {props.locations.map((location, index) => (
+        <span key={index + 100}
+              id="location.title"
+              className="list-title"
+              onClick={() => props.onFilterLocations(location.title)}
+              >{location.title}</span>
+      ))}
     </div>
   </div>
   )
