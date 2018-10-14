75afcaa93d2e746c4c4072518c54c49c60a9df15
ZumDeWald <zumdewald@gmail.com>
2018-10-14 13:56:22 -0500

    Basic outline for Search implemented
    Search button area created
    Basic CSS in place to ID areas
    Needs functionality created

--- neighborhood-map/src/App.js
+++ neighborhood-map/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import FilterList from './FilterList';
 import Map from './Map';
 import InfoWindow from './InfoWindow';
 
@@ -15,6 +16,11 @@ class App extends Component {
       {title: 'Gesthsemane', location:{lat: 31.7793143, lng: 35.2375914}},
       {title: 'Church of the Holy Sepulchre', location:{lat: 31.777207, lng: 35.231681}},
     ],
+  }
+
+  filterLocations (point) {
+    const updatedLocation = this.state.locations.filter(location => location === point);
+    this.setState({ locations: updatedLocation });
   }
 
   createInfoWindow(e, map) {
@@ -46,45 +52,48 @@ class App extends Component {
     const locations = this.state.locations;
 
     return (
-      <Map
-        id='main-map'
-        options={{
-          center: { lat:31.7053996 ,lng:35.1936877 },
-          zoom: 13
-        }}
+      <div id="map-container">
+        <FilterList />
+        <Map
+          id='main-map'
+          options={{
+            center: { lat:31.7053996 ,lng:35.1936877 },
+            zoom: 13
+          }}
 
-      //On creating a map instance, add markers/infoWindows
-      //This setup allows for multiple Maps to be loaded
-      //independently inside the same main App if desired
-        onMapLoad={ map => {
-          //Create bounds instance
-          const bounds = new window.google.maps.LatLngBounds();
+        //On creating a map instance, add markers/infoWindows
+        //This setup allows for multiple Maps to be loaded
+        //independently inside the same main App if desired
+          onMapLoad={ map => {
+            //Create bounds instance
+            const bounds = new window.google.maps.LatLngBounds();
 
-          //Loop over state and create marker info for each
-          //location and push into new array
-          //Then add listener for each individual marker
-          locations.forEach((place, index) => {
-            let position = place.location;
-            let title = place.title;
-            let marker = new window.google.maps.Marker({
-              map: map,
-              position: position,
-              title: title,
-              animation: window.google.maps.Animation.DROP,
-              id: index
+            //Loop over state and create marker info for each
+            //location and push into new array
+            //Then add listener for each individual marker
+            locations.forEach((place, index) => {
+              let position = place.location;
+              let title = place.title;
+              let marker = new window.google.maps.Marker({
+                map: map,
+                position: position,
+                title: title,
+                animation: window.google.maps.Animation.DROP,
+                id: index
+              });
+              //Add marker to new array
+              markers.push(marker);
+              //Extend boundry of map to incorporate each marker
+              bounds.extend(marker.position);
+              marker.addListener('click', () => {
+                this.createInfoWindow(marker, map);
+              });
             });
-            //Add marker to new array
-            markers.push(marker);
-            //Extend boundry of map to incorporate each marker
-            bounds.extend(marker.position);
-            marker.addListener('click', () => {
-              this.createInfoWindow(marker, map);
-            });
-          });
-          //Fit map to extended bounds
-          map.fitBounds(bounds);
-        }}
-      />
+            //Fit map to extended bounds
+            map.fitBounds(bounds);
+          }}
+        />
+      </div>
     );
   }
 
