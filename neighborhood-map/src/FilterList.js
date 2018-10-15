d17a8a170610aacf148b242c56028855b1dc65a1
ZumDeWald <zumdewald@gmail.com>
2018-10-15 09:37:16 -0500

    Styling done to filter bar
    Filter bar styled
    All selections showing

--- neighborhood-map/src/FilterList.js
+++ neighborhood-map/src/FilterList.js
@@ -6,7 +6,26 @@ function FilterList(props) {
   return(
     //Empty div to be populated by props passed down
   <div id="filter-bar">
-    <div id="list-title">Filter Selections</div>
+    <div id="filter-title">Filter:</div>
+    <select id="filter-options">
+      <option value="See All Locations"
+              className="filter-option"
+              onChange={(e) => props.onFilterMarker(e.value)}
+              >See All Locations</option>
+      {props.locations.map((location, index) => (
+        <option key={index + 100}
+            value={location}
+            id="index"
+            className="filter-option"
+            >{location.title}</option>
+      ))}
+      {/* <option value="Bethlehem" className="filter-option">Bethlehem</option>
+      <option value="Nazareth" className="filter-option">Nazareth</option>
+      <option value="Capernaum" className="filter-option">Capernaum</option>
+      <option value="Gesthsemane" className="filter-option">Gesthsemane</option>
+      <option value="Church of the Holy Sepulchre" className="filter-option">Church of the Holy Sepulchre</option> */}
+    </select>
+    {/* <div id="list-title">Filter Selections</div>
     <ul id="drop-list">
       {props.locations.map((location, index) => (
         <li key={index + 100}
@@ -19,7 +38,7 @@ function FilterList(props) {
     <div className="show-all-button"
           onClick={() => props.showAllLocations()}>
       See All
-    </div>
+    </div> */}
   </div>
   )
 }
